e67ff204cf926e80f4ab49e02c06327db13f2aa9
Oliver Coimbra <olivercoimbra@gmail.com>
2021-12-09 11:43:31 -0400

    Add new features to files

---
+++ src/events.js
@@ -0,0 +1,50 @@
+import _ from 'lodash';
+// eslint-disable-next-line import/no-cycle
+import { addNewTask } from './class';
+// eslint-disable-next-line import/no-cycle
+import {
+  saveTaskToLocal,
+  tasks,
+  removeAll,
+  populateList,
+  listContainer,
+} from './index';
+
+export const inputTask = document.querySelector('.input-tasks');
+const clearTaskBtn = document.querySelector('.clear-btn');
+
+export const changeState = (value) => {
+  const checkBox = document.querySelectorAll('.checkbox');
+  _.forEach(checkBox, (check, i) => {
+    check.addEventListener('click', () => {
+      value[i].completed = check.checked;
+      saveTaskToLocal(value);
+      const itemDesc = Array.from(
+        document.querySelectorAll('.item-description'),
+      );
+      if (value[i].completed === check.checked) {
+        itemDesc[i].classList.add('item-description-done');
+      } else {
+        itemDesc[i].classList.remove('item-description-done');
+        itemDesc[i].contentEditable = 'true';
+      }
+    });
+  });
+};
+
+inputTask.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    if (inputTask.value !== '') {
+      addNewTask();
+    }
+  }
+});
+
+clearTaskBtn.addEventListener('click', () => {
+  removeAll();
+  listContainer.innerHTML = '';
+  populateList(tasks);
+  // eslint-disable-next-line
+  location.reload();
+});
